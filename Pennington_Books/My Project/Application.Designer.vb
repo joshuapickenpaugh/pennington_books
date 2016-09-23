1ad806305140d1bf9ac4dc341a33338f0ec049d4
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-23 17:36:33 -0500

    First commit.

--- Pennington_Books/My Project/Application.Designer.vb
+++ Pennington_Books/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Pennington_Books.Form1
+            Me.MainForm = Global.Pennington_Books.frmMain
         End Sub
     End Class
 End Namespace
